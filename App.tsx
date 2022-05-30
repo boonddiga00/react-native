7ef568733c2bece6d6e38797af626db3bf242e94
boonddiga00 <boonddiga00@naver.com>
2022-05-31 00:31:56 +0900

    Layout Style

--- App.tsx
+++ App.tsx
@@ -3,18 +3,48 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, Byungjin</Text>
+    <View style={style.container}>
+      <View style={style.city}>
+        <Text style={style.cityName}>Seoul</Text>
+      </View>
+      <View style={style.weather}>
+        <View style={style.day}>
+          <Text style={style.temp}>27</Text>
+          <Text style={style.description}>Sunny</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flex: 1.1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.5,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 150,
+  },
+  description: {
+    marginTop: -10,
+    fontSize: 40,
+    fontWeight: "600",
   },
 });
